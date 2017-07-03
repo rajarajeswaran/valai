16fad2e06ad594753cdf92bddd874a5e2f7ec2ae
raja <raja>
2017-07-03 20:48:15 +0100

    dd

--- qws/qwp/Program.cs
+++ qws/qwp/Program.cs
@@ -6,7 +6,7 @@ namespace qwp
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");;;
+            Console.WriteLine("Hello World!!!!");
         }
     }
 }
